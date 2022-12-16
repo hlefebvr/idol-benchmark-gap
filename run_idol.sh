b21675f0c2e1fd8335e086c5e77ce81d02a9e4ce
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-12-16 18:53:53 +0100

    add logs to output

--- run_idol.sh
+++ run_idol.sh
@@ -10,6 +10,6 @@ echo "Solving using idol"
 for FILE in $INSTANCES/*
 do
   echo "$(date) ${FILE}"
-  echo "$(date) ${FILE}" >> history.log
-  $BUILD_DIRECTORY/GAP/idol/benchmark_idol_gap $FILE >> history.log 2>&1
+  echo "$(date) ${FILE}" >> reports/history_idol_GAP.log
+  $BUILD_DIRECTORY/GAP/idol/benchmark_idol_gap $FILE >> reports/history_idol_GAP.log 2>&1
 done
