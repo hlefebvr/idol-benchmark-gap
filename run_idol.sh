b063313a3c69cca4096d57e3ac33b8456491521c
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-12-19 11:20:54 +0100

    add forgotten extra parameter

--- run_idol.sh
+++ run_idol.sh
@@ -9,7 +9,7 @@ INSTANCES=$1
 echo "Solving using idol"
 for FILE in $INSTANCES/*
 do
-  echo "$(date) ${FILE} ${2} ${3} ${4} ${5}"
-  echo "$(date) ${FILE} ${2} ${3} ${4} ${5}" >> reports/history_idol_GAP.log
-  $BUILD_DIRECTORY/GAP/idol/benchmark_idol_gap $FILE $2 $3 $4 $5 >> reports/history_idol_GAP.log 2>&1
+  echo "$(date) ${FILE} ${2} ${3} ${4} ${5} ${6}"
+  echo "$(date) ${FILE} ${2} ${3} ${4} ${5} ${6}" >> reports/history_idol_GAP.log
+  $BUILD_DIRECTORY/GAP/idol/benchmark_idol_gap $FILE $2 $3 $4 $5 $6 >> reports/history_idol_GAP.log 2>&1
 done
