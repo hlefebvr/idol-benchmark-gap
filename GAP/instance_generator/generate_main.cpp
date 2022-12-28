c37be5b6fea92cf418b2f212f9422c6c98ed1f89
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-12-28 11:00:53 +0100

    add instances (2,40)

--- GAP/instance_generator/generate_main.cpp
+++ GAP/instance_generator/generate_main.cpp
@@ -16,13 +16,13 @@ int main(int t_argc, const char** t_argv) {
 
     const std::vector<std::pair<unsigned int, unsigned int>> sizes = {
             //{ 2, 30 },
-            //{ 2, 40 },
-            { 3, 30 },
-            { 3, 40 },
+            { 2, 40 },
+            //{ 3, 30 },
+            //{ 3, 40 },
     };
 
     for (const auto& [n_agents, n_jobs] : sizes) {
-        for (unsigned int k = 0 ; k < 5 ; ++k) {
+        for (unsigned int k = 0 ; k < 20 ; ++k) {
             auto instance = Problems::GAP::generate_instance_Chu_and_Beasley_1997_C(n_agents, n_jobs);
 
             const std::string filename = output_dir + "/instance_n" + std::to_string(n_agents) + "_" + std::to_string(n_jobs) + "__" + std::to_string(k) + ".txt";
