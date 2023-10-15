5a5b696d6b3ffbaac994a7156f844e9a434106be
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-10-15 23:12:20 +0200

    use UUID in coluna

--- run_coluna.sh
+++ run_coluna.sh
@@ -1,14 +1,15 @@
-if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
-  echo "One argument required: path_to_instances"
+if [ "$#" -ne 2 ] || ! [ -d "$1" ]; then
+  echo "Argument required: path_to_instances UUID"
   exit 1
 fi
 
 BUILD_DIRECTORY=./cmake-build-debug
 INSTANCES=$1
+UUID=$2
 
 echo "Solving using coluna"
 
 for FILE in $INSTANCES/*
 do
-  timeout 300s julia GAP/coluna/main.jl glpk $FILE || echo "${FILE},coluna,1,0,0,500,1,TIME_LIMIT,,,,,," >> results_GAP_coluna.csv
+  timeout 300s julia GAP/coluna/main.jl glpk $FILE || echo "${FILE},coluna,1,0,0,500,1,TIME_LIMIT,,,,,," >> "results_GAP_coluna__${UUID}.csv"
 done
