0ebd53edfda5b4e0eea02a780fc66753ff11b83d
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-10-15 23:00:29 +0200

    use hard time limit of 300s for coluna

--- run_coluna.sh
+++ run_coluna.sh
@@ -10,5 +10,5 @@ echo "Solving using coluna"
 
 for FILE in $INSTANCES/*
 do
-  timeout 320s julia GAP/coluna/main.jl glpk $FILE || echo "${FILE},coluna,1,0,0,500,1,TIME_LIMIT,,,,,," >> results_GAP_coluna.csv
+  timeout 300s julia GAP/coluna/main.jl glpk $FILE || echo "${FILE},coluna,1,0,0,500,1,TIME_LIMIT,,,,,," >> results_GAP_coluna.csv
 done
